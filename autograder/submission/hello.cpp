e2329dbcf2acdf3813c3c5a2f892a1b2210464f3
plexiform <haaris.nyc@gmail.com>
2021-02-23 21:39:06 -0500

    create autograder directories

---
+++ autograder/submission/hello.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Haaris Siddiqui on 2/23/21.
+//  Copyright © 2021 Haaris Siddiqui. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout<<"Hello World"<<endl;
+    return 0;
+ 
+}
