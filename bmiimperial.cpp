c5fa6ccb67ea3d3b63657e307ee629ffa6d2e15e
plexiform <haaris.nyc@gmail.com>
2021-04-08 11:06:55 -0400

    submit bmiimperial.cpp

---
+++ bmiimperial.cpp
@@ -0,0 +1,26 @@
+//
+//  bmiimperial.cpp
+//  
+//
+//  Created by Haaris Siddiqui on 4/8/21.
+//
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    float weightLbs, heightInches, weightImp, heightImp, bmi, roundedBMI;
+    
+    std::cout<<"Please  enter weight  in  pounds: ";
+    std::cin>>weightLbs;
+    std::cout<<"Please  enter height  in  inches: ";
+    std::cin>>heightInches;
+    
+    weightImp = weightLbs * 0.453592;
+    heightImp = heightInches * 0.0254;
+    
+    bmi = weightImp/(heightImp * heightImp);
+    roundedBMI = ceil(bmi*100)/100;
+    std::cout<<"BMI is: "<<roundedBMI;
+    return 0;
+}
