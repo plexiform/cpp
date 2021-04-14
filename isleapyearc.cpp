b6cb024b50e76649c05895ce6e231287958db42c
plexiform <haaris.nyc@gmail.com>
2021-04-13 20:55:23 -0400

    submit isleapyear

---
+++ isleapyearc.cpp
@@ -0,0 +1,26 @@
+//
+//  isleapyearc.cpp
+//  
+//
+//  Created by Haaris Siddiqui on 4/13/21.
+//
+
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    int year;
+    year = atoi(argv[2]);
+    if (year % 4 != 0) {
+        std::cout<<year<<" was not a leap year";
+    }
+    else if (year % 100 != 0) {
+        std::cout<<year<<" was a leap year";
+    }
+    else if (year % 400 != 0) {
+        std::cout<<year<<" was not a leap year";
+    }
+    else {
+        std::cout<<year<<" was a leap year";
+    }
+    return 0;
+}
---
+++ isleapyearc.hpp
@@ -0,0 +1,13 @@
+//
+//  isleapyearc.hpp
+//  
+//
+//  Created by Haaris Siddiqui on 4/13/21.
+//
+
+#ifndef isleapyearc_hpp
+#define isleapyearc_hpp
+
+#include <stdio.h>
+
+#endif /* isleapyearc_hpp */
